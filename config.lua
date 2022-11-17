09baa2d72fadcf3b437f18ccc8c52a8a95bf1c6e
Moldrok <alessio.aquilano@outlook.it>
2022-11-17 12:55:31 +0100

    v1.0.0

---
+++ config.lua
@@ -0,0 +1,76 @@
+MBT = {}
+
+MBT.Coords = {
+	vector3(1026.88, 801.66, 25.88), --lvl_01 Iakko
+    vector3(1780.18, -274.04, 20.66), --lvl_02 Iakko
+    vector3(356.42, 5530.52, 14.37), --lvl_03 Iakko
+    vector3(-2284.94, 1432.59, 81.70) --lvl_04 Iakko
+}
+
+MBT.FallingPoint = 0.0
+
+MBT.RandomExitPoint = {
+    vector3(-3090.44, 3322.13, 11.41),
+    vector3(1163.97, 5964.91, 407.65),
+    vector3(-557.24, 1890.37, 123.75),
+    vector3(-54.04, 33.76, 1924.76),
+    vector3(-159.57, -975.7, 115.28),
+    vector3(1753.06, 4228.73, -5.85)
+}
+
+MBT.BackRooms = {
+    {
+        Type = "Exit",
+        Coords = vector3(310.52, 5522.78, 14.51),
+        Range = 1.5,
+        Label = "Passes through"
+    },
+    {
+        Type = "Exit",
+        Coords = vector3(1779.16, -260.81, 20.86),
+        Range = 1.5,
+        Label = "Passes through"
+    },
+    {
+        Type = "Exit",
+        Coords = vector3(-2301.72, 1416.78, 81.92),
+        Range = 1.5,
+        Label = "Passes through"
+    },
+    {
+        Type = "Exit",
+        Coords = vector3(1017.94, 800.23, 25.92),
+        Range = 1.5,
+        Label = "Passes through"
+    },
+    {
+        Type = "Enter",
+        Coords = vector3(1112.08, 2188.65, 46.39),
+        Range = 2,
+        Label = "Passes through"
+    },
+    {
+        Type = "Enter",
+        Coords = vector3(-220.38, 3649.13, 51.75),
+        Range = 2,
+        Label = "Passes through"
+    },
+    {
+        Type = "Enter",
+        Coords = vector3(-440.85, 1598.89, 358.47),
+        Range = 2,
+        Label = "Passes through"
+    },
+    {
+        Type = "Enter",
+        Coords = vector3(1665.08, -28.03, 196.94),
+        Range = 2,
+        Label = "Passes through"
+    },
+    {
+        Type = "Enter",
+        Coords = vector3(3426.76, 5174.49, 7.41),
+        Range = 2,
+        Label = "Passes through"
+    }
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "Malibù Tech Team"
+description "mbt_backrooms"
+
+version '1.0.0'
+
+this_is_a_map 'yes'
+files {
+  "interiorproxies.meta"
+}
+
+client_scripts {
+  "config.lua",
+  "client.lua"
+}
